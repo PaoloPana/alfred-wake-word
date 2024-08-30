fbc5a054003de2b795d73274120492ddd156fe46
Paolo Panarese <panarese.paolo@gmail.com>
2024-08-30 19:49:05 +0200

    reworked with events

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,16 @@
 use std::collections::LinkedList;
-use alfred_rs::connection::Sender;
 use alfred_rs::error::Error;
 use alfred_rs::interface_module::InterfaceModule;
 use alfred_rs::log::debug;
 use alfred_rs::message::{Message, MessageType};
+use alfred_rs::module::Module;
 use alfred_rs::tokio;
 use porcupine::PorcupineBuilder;
 use pv_recorder::PvRecorderBuilder;
 
 const MODULE_NAME: &'static str = "wake-word";
 
-#[tokio::main]
-async fn main() -> Result<(), Error> {
-    env_logger::init();
-    let module = InterfaceModule::new(MODULE_NAME.to_string()).await?;
-    let mut publisher = module.connection.publisher;
-    let access_key = module.config.get_module_value("porcupine_access_key".to_string()).expect("Porcupine access-key not found");
+fn get_libraries(module: &InterfaceModule) -> (Option<String>, Option<String>) {
     let library_path = module.config.get_module_value("library_path".to_string());
     let mut porcupine_library_path = None;
     let mut recorder_library_path = None;
@@ -24,13 +19,22 @@ async fn main() -> Result<(), Error> {
         porcupine_library_path = Some(library_path.clone() + "libpv_porcupine.so");
         recorder_library_path = Some(library_path.clone() + "libpv_recorder.so");
     }
-    porcupine_library_path = module.config.get_module_value("porcupine_library_path".to_string()).or(porcupine_library_path);
-    recorder_library_path = module.config.get_module_value("recorder_library_path".to_string()).or(recorder_library_path);
+    (
+        module.config.get_module_value("porcupine_library_path".to_string()).or(porcupine_library_path),
+        module.config.get_module_value("recorder_library_path".to_string()).or(recorder_library_path)
+    )
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    env_logger::init();
+    let mut module = InterfaceModule::new(MODULE_NAME.to_string()).await?;
+    let access_key = module.config.get_module_value("porcupine_access_key".to_string()).expect("Porcupine access-key not found");
+    let (porcupine_library_path, recorder_library_path) = get_libraries(&module);
     let device_name = module.config.get_module_value("device_name".to_string());
 
     let ppn_model = module.config.get_module_value("ppn_model".to_string()).expect("Porcupine model file not found");
     let lang_model = module.config.get_module_value("lang_model".to_string()).expect("Porcupine model file not found");
-    let callback_topic = module.config.get_module_value("callback_topic".to_string()).expect("Callback not found");
 
     let mut porcupine_builder = PorcupineBuilder::
         new_with_keyword_paths(access_key, &[ppn_model]);
@@ -65,14 +69,8 @@ async fn main() -> Result<(), Error> {
         let keyword_index = porcupine.process(&frame).unwrap();
         if keyword_index >= 0 {
             let mut message = Message::empty();
-            message.response_topics = LinkedList::from([
-                "stt".to_string(),
-                "openai".to_string(),
-                "tts".to_string(),
-                "audio_out".to_string(),
-            ]);
             message.message_type = MessageType::AUDIO;
-            publisher.send(callback_topic.clone(), &message).await?;
+            module.send_event(MODULE_NAME.to_string(), "triggered".to_string(), &message).await?;
             debug!("Detected {}", keyword_index);
         }
     }
